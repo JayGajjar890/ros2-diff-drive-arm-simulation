5ec73d3b772f1ec3992f2c150bee3e9f2c7d55bb
Jay Gajjar <jaygajjar890@gmail.com>
2026-04-26 15:00:15 +0530

    Initial commit: ROS2 Gazebo simulation of 2-wheel differential drive robot with simple 2-link arm

---
+++ src/my_robot_description/launch/display.launch.py
@@ -0,0 +1,12 @@
+# from launch import LaunchDescription
+# from launch_ros.parameter_descriptions import ParameterValue
+# from launch_ros.actions import Node
+# from launch.substitutions import Command
+# import os
+# from ament_index_python.packages import get_package_share_path
+
+# def generate_launch_description():
+
+#     urdf_path=os.path.join(get_package_share_path('my_robot_description'),'urdf','my_robot_urdf')
+
+#     robot_description=Parame
